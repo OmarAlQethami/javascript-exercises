b3d99feb84b9ecbb77891bc1cd9b4ea42029af88
OmarAlQethami <omaralqethami@gmail.com>
2022-07-26 14:25:19 +0300

    Exercises 8-9-10 Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  var re = /[\W_]/g;
+  var lowRegStr = text.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join(''); 
+  return reverseStr === lowRegStr;
 
 };
 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let seq = [];
+    if(num < 0) {
+        return "OOPS";
+    }
+    for(let i = 0; i <= num; i++) {
+        if(i === 0 || i === 1) {
+            seq[i] = 1;
+            continue;
+        }
+        seq[i] = seq[i - 1] + seq[i - 2];
+    }
+    return seq[num - 1];
 };
 
 // Do not edit below this line
